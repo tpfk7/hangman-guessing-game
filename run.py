f0a708356edfc6d5f1abaecb49a7cbbf914eb531
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-04-03 21:43:22 +0000

    Add partial solution placeholder

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ import random
 
 mystery_words = ["TIGER","SHARK","LION"]
 mystery_word = random.choice(mystery_words)
+
+wrong_guesses = []
+partial_solution = "_" * len(mystery_word)
 print(mystery_word)
 
-print(data.gallows[0])
+print(data.gallows[len(wrong_guesses)])
+print(f"Word: {partial_solution}")
