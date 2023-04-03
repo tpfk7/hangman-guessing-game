5b83281c10cca722971319f868dd58bcbd3995d7
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-04-03 22:13:21 +0000

    Add fix to while loop

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 import data
 import random
 
-mystery_words = ["TIGER","SHARK","LION"]
+mystery_words = ["TIGER", "SHARK", "LION"]
 mystery_word = random.choice(mystery_words)
 
 wrong_guesses = []
 partial_solution = "_" * len(mystery_word)
-print(mystery_word)
 
 print(data.gallows[len(wrong_guesses)])
 print(f"Word: {partial_solution}")
+
+while len(wrong_guesses) < len(data.gallows) -1:
+    c = input("Your guess: ").upper()
+    if c in mystery_word:
+        pass
+    else:
+        wrong_guesses.append(c)
+    print(data.gallows[len(wrong_guesses)])
