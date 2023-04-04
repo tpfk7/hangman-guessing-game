81a8db98a07356d996012688b66703c53d83351a
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-04-04 16:35:39 +0000

    "Reformat page functions"

--- run.py
+++ run.py
@@ -1,19 +1,53 @@
-import data
+import os
 import random
+from data import GALLOWS as gallows
+
+
+def clear():
+    """
+    Clear function to clean-up the terminal so things don't get messy.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 mystery_words = ["TIGER", "SHARK", "LION"]
-mystery_word = random.choice(mystery_words)
-
+mystery_word = ""
 wrong_guesses = []
-partial_solution = "_" * len(mystery_word)
 
-print(data.gallows[len(wrong_guesses)])
-print(f"Word: {partial_solution}")
 
-while len(wrong_guesses) < len(data.gallows) -1:
-    c = input("Your guess: ").upper()
-    if c in mystery_word:
-        pass
-    else:
-        wrong_guesses.append(c)
-    print(data.gallows[len(wrong_guesses)])
+def get_mystery_word():
+    mystery_word = random.choice(mystery_words)
+    return mystery_word
+
+
+def validate_letter(guess):
+    if not guess.isalpha() or len(guess) != 1:
+        print(f"\n{guess} is not valid. Please enter a letter.")
+        guess = input("Your guess: ").upper()
+        validate_letter(guess)
+        return False
+    return True
+
+
+def start_game():
+    print(gallows[len(wrong_guesses)])
+    mystery_word = get_mystery_word()
+    partial_solution = "_" * len(mystery_word)
+
+    while len(wrong_guesses) < len(gallows) - 1:
+        print(mystery_word)  # TODO: test only
+        print(f"Word: {partial_solution}")
+        guess = input("Your guess: ").upper()
+        if validate_letter(guess):
+            pass
+        clear()
+        if guess in mystery_word:
+            pass
+        else:
+            wrong_guesses.append(guess)
+        print(gallows[len(wrong_guesses)])
+
+
+if __name__ == "__main__":
+    clear()
+    start_game()
